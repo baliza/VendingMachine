c8c2f701a6a9eeb21c61e8c932eb41c130bb22f8
Eduardo Salvador <eduardo.salvador@rakuten.com>
2016-11-22 16:23:59 +0000

    module 1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var VedingMachine = (function () {
+    function VedingMachine() {
+    }
+    return VedingMachine;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new VedingMachine();
+//# sourceMappingURL=app.js.map
